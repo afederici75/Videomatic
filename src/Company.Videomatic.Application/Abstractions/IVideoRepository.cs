5ceedbcbc97019d8e1acb7b20be9cb7c0729c85d
Alessandro Federici <afederici75@gmail.com>
2023-04-27 08:58:23 -0400

    About to introduce Ardalis IRepository

--- src/Company.Videomatic.Application/Abstractions/IVideoRepository.cs
+++ src/Company.Videomatic.Application/Abstractions/IVideoRepository.cs
@@ -1,16 +1,20 @@
 ﻿using Ardalis.Specification;
-using Company.Videomatic.Application.Features.Videos.Queries.GetVideos;
 using Company.Videomatic.Domain.Model;
 using Company.Videomatic.Domain.Specifications;
-using System.Linq.Expressions;
-using System.Runtime.CompilerServices;
 
 namespace Company.Videomatic.Application.Abstractions;
 
 public interface IVideoRepository
 {
+    Ardalis.Specification.IReadRepositoryBase<object> readRepositoryBase();
+
     Task<int> UpdateVideoAsync(Video video);
     Task<bool> DeleteVideoAsync(int id);
     Task<Video> GetVideoByIdAsync(GetVideoSpecification spec);
     Task<IEnumerable<Video>> GetVideosAsync(GetVideosSpecification spec);
+}
+
+public interface IRepository<T> : IRepositoryBase<T> 
+    where T : class
+{ 
 }
