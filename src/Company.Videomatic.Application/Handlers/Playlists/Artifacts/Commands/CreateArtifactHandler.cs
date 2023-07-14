a8b5880cf093ef29b92a40274db2112928d4b124
Alessandro Federici <afederici75@gmail.com>
2023-07-14 13:54:59 -0400

    some ignored files .gitignore AGAIN!

---
+++ src/Company.Videomatic.Application/Handlers/Playlists/Artifacts/Commands/CreateArtifactHandler.cs
@@ -0,0 +1,30 @@
+﻿using Ardalis.Specification;
+
+namespace Company.Videomatic.Application.Handlers.Artifacts.Commands;
+
+public class NewCreateArtifactHandler : IRequestHandler<CreateArtifactCommand, Result<CreateArtifactResponse>>
+{
+    public NewCreateArtifactHandler(IServiceProvider serviceProvider, IMapper mapper)
+    {
+        ServiceProvider = serviceProvider;
+        //Repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        Mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+
+        CreateArtifactCommand cmd = new CreateArtifactCommand(1, "Name", "Type", "text...");
+        if (cmd is ICRUDCommand)
+        {
+            //long id = cmd.Id;
+        }
+
+
+    }
+
+    protected IServiceProvider ServiceProvider { get; }
+
+    protected IMapper Mapper { get; }
+
+    public Task<Result<CreateArtifactResponse>> Handle(CreateArtifactCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
