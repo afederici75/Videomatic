9446a2c496b8063790088bd81f54d0ee4c577afe
Alessandro Federici <afederici75@gmail.com>
2023-07-26 12:31:36 -0400

    I can use TagCount in my query

--- tests/Infrastructure.Tests/Data/DynamicLinqTests.cs
+++ tests/Infrastructure.Tests/Data/DynamicLinqTests.cs
@@ -27,34 +27,47 @@ public class DynamicLinqTests
 
     [Theory]
     //[InlineData("", "", null, null)]
-    [InlineData(@"(Name.Contains(""a""))", "", null, null)]
+    [InlineData($"(Name.Contains(\"a\"))", "", null, null)]
+    [InlineData($"(TagCount > 0)", "", null, null)]
+    [InlineData(null, "TagCount DESC", null, null)]
+    [InlineData(null, "ArtifactCount DESC", null, null)]
     public async Task TestDynamicLinq(string? whereText, string? orderByText, int? take = null, int? skip = null)
     {
         using var db = await DbContextFactory.CreateDbContextAsync();
 
+        var aliases = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        {
+            { "TagCount", "Tags.Count()" },
+            { "ArtifactCount", "Artifacts.Count()" },
+            { "TranscriptCount", "Transcripts.Count()" }
+        };
+        foreach (var d in aliases)
+        { 
+            whereText = whereText?.Replace(d.Key, d.Value);
+            orderByText = orderByText?.Replace(d.Key, d.Value);
+        }
+
         // --- Sets up the main query
-        IQueryable<Video> q = db.Videos.AsQueryable();
+        IQueryable<Video> q = db.Videos
+            .AsSplitQuery()
+            .AsQueryable();
 
         if (!string.IsNullOrWhiteSpace(whereText))
             q = q.Where(whereText);
 
         if (!string.IsNullOrWhiteSpace(orderByText))
             q = q.OrderBy(orderByText);
+        else
+            q = q.OrderBy("Id");
 
-        #region Paging
-        if (take.HasValue)
-            q = q.Take(take.Value);
-
-        if (skip.HasValue)
-            q = q.Skip(skip.Value);
-        #endregion
+        q = q.Take(take ?? 10).Skip(skip ?? 0);
 
         var final = q.Select(v => new VideoDTO(
         v.Id,
             v.Location,
             v.Name,
             v.Description,
-            null, //request.IncludeTags ? v.Tags.Select(t => t.Name) : null,
+            v.Tags.Select(t => t.Name),// : null,
             null,//includeThumbnail ? v.Thumbnails.Single(t => t.Resolution == preferredRes).Location : null,
             db.Artifacts.Count(a => a.VideoId == v.Id),
             db.Transcripts.Count(a => a.VideoId == v.Id),
@@ -66,10 +79,11 @@ public class DynamicLinqTests
             v.Details.VideoOwnerChannelId
             ));
 
-        var results = await q.ToListAsync();
+        var results = await final.ToListAsync();
         
-        var queryText = q.ToQueryString();
+        var queryText = final.ToQueryString();
         Output.WriteLine($"{results.Count} Result(s).");
-        Output.WriteLine($"{queryText}:\n{queryText}");        
+        Output.WriteLine($"------------------------------------");
+        Output.WriteLine($"{queryText}");
     }
 }
