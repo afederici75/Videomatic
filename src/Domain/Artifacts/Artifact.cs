ea93db596de478f68bf288a663fa366720e79e82
Alessandro Federici <afederici75@gmail.com>
2023-08-14 14:10:20 -0400

    Major cleanup of project names and namespaces simplification complete?

---
+++ src/Domain/Artifacts/Artifact.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Videos;
+
+namespace Domain.Artifacts;
+
+public class Artifact : Entity<ArtifactId>, IAggregateRoot
+{
+    public Artifact(VideoId videoId, string name, string type, string? text = null)
+    {
+        VideoId = videoId;
+        Name = Guard.Against.NullOrWhiteSpace(name, nameof(name));
+        Type = Guard.Against.NullOrWhiteSpace(type);
+        Text = text;
+    }
+
+    public VideoId VideoId { get; private set; } = default!;
+    public string Name { get; private set; } = default!;
+    public string Type { get; private set; } = default!;
+    public string? Text { get; private set; }
+    
+    #region Private
+
+    private Artifact()
+    { }
+
+    #endregion
+}
+
